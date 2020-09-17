b32bc0899c18e67f9d4a04b42d3bc31acc2e94b2
Luís Guilherme Bergamini Mendes <luisgbm@gmail.com>
2020-09-17 18:14:09 -0300

    Update lfs-system.sh

--- lfs-system.sh
+++ lfs-system.sh
@@ -11,11 +11,15 @@ begin() {
 	package_name=$1
 	package_ext=$2
 	
+	echo "[lfs-scripts] Starting build of $package_name at $(date)"
+	
 	tar xf $package_name.$package_ext
 	cd $package_name
 }
 
 finish() {
+	echo "[lfs-scripts] Finishing build of $package_name at $(date)"
+
 	cd /sources
 	rm -rf $package_name
 }
