3267f185d62908aad8e69aecbe9b329298b75ffb
Luís Mendes <luisgbm@gmail.com>
2022-09-07 17:15:31 -0300

    Fixing error in grub config

--- lfs-final.sh
+++ lfs-final.sh
@@ -229,10 +229,10 @@ set default=0
 set timeout=5
 
 insmod ext2
-set root=(hd0,2)
+set root=(hd0,1)
 
 menuentry "GNU/Linux, Linux 5.19.2-lfs-11.2" {
-        linux   /boot/vmlinuz-5.19.2-lfs-11.2 root=/dev/sda2 ro
+        linux   /boot/vmlinuz-5.19.2-lfs-11.2 root=/dev/sda1 ro
 }
 EOF
 
